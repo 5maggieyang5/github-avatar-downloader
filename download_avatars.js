bf272469dfa36c7d4bb7c6fec9c00d2d1d8dbd9b
mag <mag.it.use@gmail.com>
2018-08-07 00:53:41 +0000

    adding function of getRepoContributors

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,29 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  // ...
+  var URL = {
+    uri: 'api.github.com',
+    path: `/repos/${repoOwner}/${repoName}/contributors`
+  }
+const options = {
+  url: 'https://' + URL.uri + URL.path,
+  headers: {
+    'User-Agent': 'request'
+  }
+};
+
+  request.get(options, function (error, result) {
+
+  cb(error, result);
+  console.log('error:', error);
+  console.log('result:', result); // Print the response status code if a response was received
+  });
+}
+
+getRepoContributors('jquery', 'jquery', function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
